56fd739c0c5920865ceeaafdd9979287154b914a
Stefan F. Schouten <sfschouten@gmail.com>
2023-05-12 19:39:49 +0200

    added 1-dimensional implementation

--- README.md
+++ README.md
@@ -8,17 +8,28 @@ The downside is that it requires the mask to be kept in memory so it can be appl
 A seeded implementation works by using the same seed in the same way, so that instead of keeping the mask in memory, we only need to remember the seed.
 The potential memory savings are pretty modest, so it's important that the seeded implementation isn't any slower than the conventional implementation.
 
-## Performance
-Benchmark compares performance to conventional pytorch implementation , and to that same implementation but with `torch.compile`.
+This repository contains two implementations of seeded variational dropout, one based on a 1-dimensional kernel, and another based on a 2-dimensional kernel.
 
-![Results of running benchmark for various sizes of tensors.](https://github.com/sfschouten/seeded-variational-dropout/blob/main/performance_fp32.png)
+
+## Performance
+Benchmark compares performance of seeded implementations to a conventional pytorch implementation.
+
+training (block sizes are powers of two)                                                                        | inference
+:--------------------------------------------------------------------------------------------------------------:|:---------------------------------------------------------------------------------------------------------------:
+![](https://github.com/sfschouten/seeded-variational-dropout/blob/main/performance_report/performance_fp32.png) | ![](https://github.com/sfschouten/seeded-variational-dropout/blob/main/performance_report/performance_fp32_.png)
+
 
 The benchmark goes through tensors of shape [1 x 64 * 768] through [8192 x 64 * 768], simulating the application of dropout to representations of larger and larger contexts.
+One variant uses only block sizes that are exact powers of two, and one simulates inference by randomly sampling block sizes.
 
-With the autotune feature of Triton performance is now at least as good as regular pytorch for all sizes.
+As can be seen the 2-dimensional implementation suffers a lot from the block sizes not being exact powers of two, whereas the 1-dimensional implementation does not.
+For both implementations, with the help of the autotune feature of Triton, performance is now at least as good as regular pytorch for all sizes.
 
-Something interesting happens when I do the same thing for dtype=torch.float16 and dtype=torch.float64 though.
 
-torch.float16                                                                                                 | torch.float64
-:------------------------------------------------------------------------------------------------------------:|:------------------------------------------------------------------------------------------------------------:
-![Results for FP16](https://github.com/sfschouten/seeded-variational-dropout/blob/main/performance_fp16.png)  | ![Results for FP64](https://github.com/sfschouten/seeded-variational-dropout/blob/main/performance_fp64.png)
+The same graphs for dtype=torch.float16 and dtype=torch.float64.
+
+torch.float16                                                                                                   | torch.float64
+:--------------------------------------------------------------------------------------------------------------:|:---------------------------------------------------------------------------------------------------------------:
+![](https://github.com/sfschouten/seeded-variational-dropout/blob/main/performance_report/performance_fp16.png) | ![](https://github.com/sfschouten/seeded-variational-dropout/blob/main/performance_report/performance_fp64.png)
+
+Not totally sure, but looks to me as though the torch implementation just ignores dtypes and performs the operation as though they are all torch.float32.
--- variational_dropout.py
+++ variational_dropout.py
@@ -7,68 +7,39 @@ import triton
 import triton.language as tl
 
 
+# == 2D kernel implementation ==
+
+MAX_NR_ELEM_BITS = 15
+
 def _kernel_config_pruner(configs, nargs):
     """
     We don't want to use block sizes with dimensions larger than those of the tensor we are applying the dropout to.
     """
-    n = max(triton.next_power_of_2(nargs['N']), 16)
+    n = max(triton.next_power_of_2(nargs['N']), 1)
     m = max(triton.next_power_of_2(nargs['M']), 1)
 
     used = set()
     for config in configs:
-        block_size_m = min(m, int(config.kwargs['BLOCK_SIZE_M']))
         block_size_n = min(n, int(config.kwargs['BLOCK_SIZE_N']))
+        block_size_m = min(m, int(config.kwargs['BLOCK_SIZE_M']))
 
         if (block_size_m, block_size_n, config.num_stages, config.num_warps) in used:
             continue
-
-        if block_size_n * block_size_m < 256:
+        
+        if (block_size_n * block_size_m) < 2**9 or (block_size_n * block_size_m) > 2**MAX_NR_ELEM_BITS: 
+            #TODO test these boundaries on other GPUs
             continue
 
         used.add((block_size_m, block_size_n, config.num_stages, config.num_warps))
-        yield triton.Config({'BLOCK_SIZE_N': block_size_n, 'BLOCK_SIZE_M': block_size_m}, 
-                            num_stages=config.num_stages, num_warps=config.num_warps)
+        yield triton.Config({'BLOCK_SIZE_N': block_size_n, 'BLOCK_SIZE_M': block_size_m}, num_stages=config.num_stages, num_warps=config.num_warps)
 
 
 @triton.autotune(configs=[
-#        triton.Config({'BLOCK_SIZE_N': 1,   'BLOCK_SIZE_M': 4096}, num_warps=4),     # 4096
-        triton.Config({'BLOCK_SIZE_N': 1,   'BLOCK_SIZE_M': 8192}, num_warps=4),     # 8192
-        triton.Config({'BLOCK_SIZE_N': 1,   'BLOCK_SIZE_M': 16384}, num_warps=4),     # 16384
-#        triton.Config({'BLOCK_SIZE_N': 1,   'BLOCK_SIZE_M': 32768}, num_warps=4),     # 32768 
-#        triton.Config({'BLOCK_SIZE_N': 2,   'BLOCK_SIZE_M': 2048}, num_warps=4),     # 4096
-        triton.Config({'BLOCK_SIZE_N': 2,   'BLOCK_SIZE_M': 4096}, num_warps=4),     # 8129
-        triton.Config({'BLOCK_SIZE_N': 2,   'BLOCK_SIZE_M': 8192}, num_warps=4),     # 16384
-#        triton.Config({'BLOCK_SIZE_N': 2,   'BLOCK_SIZE_M': 16384}, num_warps=4),     # 32768 
-#        triton.Config({'BLOCK_SIZE_N': 4,   'BLOCK_SIZE_M': 1024}, num_warps=4),     # 4096
-        triton.Config({'BLOCK_SIZE_N': 4,   'BLOCK_SIZE_M': 2048}, num_warps=4),     # 8129
-        triton.Config({'BLOCK_SIZE_N': 4,   'BLOCK_SIZE_M': 4096}, num_warps=4),     # 16384
-#        triton.Config({'BLOCK_SIZE_N': 4,   'BLOCK_SIZE_M': 8192}, num_warps=4),     # 32768 
-#        triton.Config({'BLOCK_SIZE_N': 8,   'BLOCK_SIZE_M': 512},  num_warps=4),     # 4096
-        triton.Config({'BLOCK_SIZE_N': 8,   'BLOCK_SIZE_M': 1024}, num_warps=4),     # 8129
-        triton.Config({'BLOCK_SIZE_N': 8,   'BLOCK_SIZE_M': 2048}, num_warps=4),     # 16384
-#        triton.Config({'BLOCK_SIZE_N': 8,   'BLOCK_SIZE_M': 4096}, num_warps=4),     # 32768 
-#        triton.Config({'BLOCK_SIZE_N': 16,  'BLOCK_SIZE_M': 256},  num_warps=4),     # 4096
-        triton.Config({'BLOCK_SIZE_N': 16,  'BLOCK_SIZE_M': 512},  num_warps=4),     # 8129
-        triton.Config({'BLOCK_SIZE_N': 16,  'BLOCK_SIZE_M': 1024}, num_warps=4),     # 16384
-#        triton.Config({'BLOCK_SIZE_N': 16,  'BLOCK_SIZE_M': 2048}, num_warps=4),     # 32768 
-#        triton.Config({'BLOCK_SIZE_N': 32,  'BLOCK_SIZE_M': 128},  num_warps=4),     # 4096
-        triton.Config({'BLOCK_SIZE_N': 32,  'BLOCK_SIZE_M': 256},  num_warps=4),     # 8129
-        triton.Config({'BLOCK_SIZE_N': 32,  'BLOCK_SIZE_M': 512},  num_warps=4),     # 16384
-#        triton.Config({'BLOCK_SIZE_N': 32,  'BLOCK_SIZE_M': 1024}, num_warps=4),     # 32768 
-#        triton.Config({'BLOCK_SIZE_N': 64,  'BLOCK_SIZE_M': 64},   num_warps=4),     # 4096
-        triton.Config({'BLOCK_SIZE_N': 64,  'BLOCK_SIZE_M': 128},  num_warps=4),     # 8192
-        triton.Config({'BLOCK_SIZE_N': 64,  'BLOCK_SIZE_M': 256},  num_warps=4),     # 16384
-#        triton.Config({'BLOCK_SIZE_N': 64,  'BLOCK_SIZE_M': 512},  num_warps=4),    # 32768 
-#        triton.Config({'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_M': 32},   num_warps=4),     # 4096
-        triton.Config({'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_M': 64},   num_warps=4),     # 8192
-        triton.Config({'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_M': 128},  num_warps=4),     # 16384
-#        triton.Config({'BLOCK_SIZE_N': 128, 'BLOCK_SIZE_M': 256},  num_warps=4),     # 32768 
-#        triton.Config({'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_M': 16},   num_warps=4),     # 4096
-        triton.Config({'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_M': 32},   num_warps=4),     # 8192
-        triton.Config({'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_M': 64},   num_warps=4),     # 16384
-#        triton.Config({'BLOCK_SIZE_N': 256, 'BLOCK_SIZE_M': 128},  num_warps=4),     # 32768     
+        triton.Config({'BLOCK_SIZE_N': 2**n, 'BLOCK_SIZE_M': 2**m}, num_warps=4)
+        for n in range(0, 10) 
+        for m in range(0, MAX_NR_ELEM_BITS) 
     ],
-    key=['N', 'M'],
+    key=['BITS', 'N_O', 'M_O'],
     prune_configs_by={
         'early_config_prune': _kernel_config_pruner,
         'perf_model': None,
@@ -76,7 +47,7 @@ def _kernel_config_pruner(configs, nargs):
     },
 )
 @triton.jit
-def _variational_dropout_kernel(x_ptr, output_ptr, N, M, stride_n, stride_m, p, seed,
+def _variational_dropout_kernel_2D(x_ptr, output_ptr, N, M, stride_n, stride_m, p, seed, BITS, N_O, M_O,
                         BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_M: tl.constexpr):
     """
     X is of shape (N, M)
@@ -85,8 +56,8 @@ def _variational_dropout_kernel(x_ptr, output_ptr, N, M, stride_n, stride_m, p, 
     # compute memory offsets of elements handled by this instance
     pid_n = tl.program_id(axis=0)
     pid_m = tl.program_id(axis=1)
-    offs_n = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N))# % N
-    offs_m = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M))# % M
+    offs_n = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N))
+    offs_m = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M))
     offsets = (offs_n[:, None]*stride_n + offs_m[None, :]*stride_m)
    
     # load data from x
@@ -94,8 +65,7 @@ def _variational_dropout_kernel(x_ptr, output_ptr, N, M, stride_n, stride_m, p, 
     x = tl.load(x_ptr + offsets, mask=mask)
     
     # randomly prune it, applying the same mask to each column
-    blk_seed = seed + pid_n                                                     # Use the same seed for every block in a row
-    random = tl.rand(blk_seed, offs_n)
+    random = tl.rand(seed, offs_n)                                          # generate dropout mask of (N, 1), apply to each column
     x_keep = random > p
     x_keep = tl.broadcast_to(x_keep[:, None], (BLOCK_SIZE_N, BLOCK_SIZE_M))
     output = tl.where(x_keep, x / (1 - p), 0.0)
@@ -103,29 +73,92 @@ def _variational_dropout_kernel(x_ptr, output_ptr, N, M, stride_n, stride_m, p, 
     # write-back
     tl.store(output_ptr + offsets, output, mask=mask)
 
-def _variational_dropout_launch(x, p, seed):
+
+def _variational_dropout_2D_launch(x, p, seed):
     output = torch.empty_like(x)
     assert x.is_contiguous() and len(x.shape) == 2
     N, M = x.shape
     grid = lambda meta: (triton.cdiv(N, meta['BLOCK_SIZE_N']), triton.cdiv(M, meta['BLOCK_SIZE_M']))
-    _variational_dropout_kernel[grid](x, output, N, M, x.stride(0), x.stride(1), p, seed)
+    _variational_dropout_kernel_2D[grid](
+            x, output, N, M, x.stride(0), x.stride(1), p, seed, torch.finfo(x.dtype).bits, int(math.log2(N)), int(math.log2(M)))
     return output
 
-class SeededVariationalDropout(torch.autograd.Function):
+class SeededVariationalDropout2D(torch.autograd.Function):
     @classmethod
     def forward(cls, ctx, x, p, seed):
         ctx.p = p
         ctx.seed = seed
-        return _variational_dropout_launch(x, p, seed)
+        return _variational_dropout_2D_launch(x, p, seed)
 
     @classmethod
     def backward(cls, ctx, dy):
         p = ctx.p
         seed = ctx.seed
-        return _variational_dropout_launch(dy, p, seed), None, None
+        return _variational_dropout_2D_launch(dy, p, seed), None, None
 
-def _torch_variational_dropout(x, p, seed):
-    torch.manual_seed(seed)
+
+
+
+# == 1D kernel implementation ==
+
+@triton.autotune(configs=[
+        triton.Config({'BLOCK_SIZE': 2**n}, num_warps=4)
+        for n in range(4, 13) 
+    ], key=['BITS', 'N_ELEM_O'] 
+)
+@triton.jit
+def _variational_dropout_kernel_1D(x_ptr, output_ptr, n_elem, n_cols, p, seed, BITS, N_ELEM_O, BLOCK_SIZE: tl.constexpr):
+    """
+
+    """
+    # compute memory offsets of elements handled by this instance
+    pid_n = tl.program_id(axis=0)
+    block_start = pid_n * BLOCK_SIZE 
+    
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elem
+    x = tl.load(x_ptr + offsets, mask=mask)
+
+    if n_cols > 1:
+        rand_offsets = offsets // n_cols
+    else:
+        rand_offsets = offsets
+    # convert offsets into row indices, and use those resulting in the same seed being used for the same rows
+    random = tl.rand(seed, rand_offsets)
+    x_keep = random > p
+    
+    output = tl.where(x_keep, x / (1 - p), 0.0)
+    tl.store(output_ptr + offsets, output, mask=mask)
+
+
+def _variational_dropout_1D_launch(x, p, seed):
+    output = torch.empty_like(x)
+    assert x.is_contiguous() and len(x.shape) == 2
+    N, M = x.shape
+    n_elem = N * M
+    grid = lambda meta: (triton.cdiv(n_elem, meta['BLOCK_SIZE']),)
+    _variational_dropout_kernel_1D[grid](x, output, n_elem, M, p, seed, torch.finfo(x.dtype).bits, int(math.log2(n_elem)))
+    return output
+
+
+
+class SeededVariationalDropout1D(torch.autograd.Function):
+    @classmethod
+    def forward(cls, ctx, x, p, seed):
+        ctx.p = p
+        ctx.seed = seed
+        return _variational_dropout_1D_launch(x, p, seed)
+
+    @classmethod
+    def backward(cls, ctx, dy):
+        p = ctx.p
+        seed = ctx.seed
+        return _variational_dropout_1D_launch(dy, p, seed), None, None
+
+def _torch_variational_dropout(x, p, seed=None):
+    if not seed:
+        seed = random.randrange(int(1e6))
+        torch.manual_seed(seed)
     ones = x.data.new_ones(x.shape[0])
     dropout_mask = torch.nn.functional.dropout(ones, p=p)
     return dropout_mask.unsqueeze(1) * x
@@ -135,7 +168,11 @@ def variational_dropout(x, p, seed=None):
         seed = random.randrange(int(1e6))
 
     if x.is_cuda:
-        return SeededVariationalDropout.apply(x, p, seed)
+        M = x.shape[-1]
+        if M & (M-1) == 0: # check if power of 2
+            return SeededVariationalDropout2D.apply(x, p, seed)
+        else:
+            return SeededVariationalDropout1D.apply(x, p, seed)
     else:
         return _torch_variational_dropout(x, p, seed)
 
@@ -144,64 +181,87 @@ def variational_dropout(x, p, seed=None):
 # == testing code ==
 N = 64 * 768
 
-@triton.testing.perf_report(
-    triton.testing.Benchmark(
+
+def _create_benchmark(x_vals, name, **kwargs):
+    return triton.testing.Benchmark(
         x_names=['size'],  # Argument names to use as an x-axis for the plot.
-        x_vals=[
-            #int(2 ** (i + random.random())) for i in range(0, 12, 1)
-            2 ** i for i in range(0, 12, 1)
-        ],  # Different possible values for `x_name`.
+        x_vals=x_vals,
         x_log=True,  # x axis is logarithmic.
         line_arg='provider',  # Argument name whose value corresponds to a different line in the plot.
-        line_vals=['triton', 'torch', 'torch-compile'],  # Possible values for `line_arg`.
-        line_names=['Triton', 'Torch', 'Torch w/ compile'],  # Label name for the lines.
-        styles=[('blue', '-'), ('green', '-'), ('red', '-')],  # Line styles.
+        line_vals=['triton-1d', 'triton-2d', 'torch', 'torch-compile'],  # Possible values for `line_arg`.
+        line_names=['Triton1D', 'Triton2D', 'Torch', 'Torch w/ compile'],  # Label name for the lines.
+        styles=[('blue', '-'), ('green', '-'), ('red', '-'), ('orange', '-')],  # Line styles.
         ylabel='GB/s',  # Label name for the y-axis.
-        plot_name='variational-dropout-performance',  # Name for the plot. Used also as a file name for saving the plot.
-        args={},  # Values for function arguments not in `x_names` and `y_name`.
+        plot_name=name,  # Name for the plot. Used also as a file name for saving the plot.
+        args=kwargs # Values for function arguments not in `x_names` and `y_name`.
     )
-)
-def benchmark(size, provider):
-    size2 = (N, size)
-    size = N * size
-    print(f"{provider:<15} {size2} ({size})")
 
-    #x = torch.rand(size2, device='cuda', dtype=torch.float16)
-    x = torch.rand(size2, device='cuda', dtype=torch.float32)
-    #x = torch.rand(size2, device='cuda', dtype=torch.float64)
+def powers_of_two(start, end, noisy):
+    return [int(2**(i+(random.random() if noisy else 0))) for i in range(start, end)]
+
+
+@triton.testing.perf_report([
+    _create_benchmark(powers_of_two(0,14,False), 'performance_fp16', dtype=torch.float16),
+    _create_benchmark(powers_of_two(0,13,False), 'performance_fp32', dtype=torch.float32),
+    _create_benchmark(powers_of_two(0,12,False), 'performance_fp64', dtype=torch.float64),
+    _create_benchmark(powers_of_two(0,13,True),  'performance_fp16_noisy', dtype=torch.float16),
+    _create_benchmark(powers_of_two(0,12,True),  'performance_fp32_noisy', dtype=torch.float32),
+    _create_benchmark(powers_of_two(0,11,True),  'performance_fp64_noisy', dtype=torch.float64),
+])
+def _benchmark(size, provider, dtype):
+    x = torch.empty((N,size), device='cuda', dtype=dtype)
+    
+    n_elements = x.shape[0] * x.shape[1]
+    print(f"{provider:<15} {str(tuple(x.shape)):<14} ({n_elements})")
+
     p = 0.5
-    seed = 1234
     quantiles = [0.5, 0.2, 0.8]
     if provider == 'torch':
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: _torch_variational_dropout(x, p, seed), quantiles=quantiles)
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: _torch_variational_dropout(x, p), quantiles=quantiles)
     if provider == 'torch-compile':
         drop_fn = torch.compile(_torch_variational_dropout)
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: drop_fn(x, p, seed), quantiles=quantiles)
-    if provider == 'triton':
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: variational_dropout(x, p, seed), quantiles=quantiles)
-    gbps = lambda ms: 12 * size / ms * 1e-6
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: drop_fn(x, p), quantiles=quantiles)
+    if provider == 'triton-1d':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: SeededVariationalDropout1D.apply(x, p, random.randrange(int(1e6))), quantiles=quantiles)
+    if provider == 'triton-2d':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: SeededVariationalDropout2D.apply(x, p, random.randrange(int(1e6))), quantiles=quantiles)
+    gbps = lambda ms: n_elements * (torch.finfo(dtype).bits / 8) / ms * 1e-6
     return gbps(ms), gbps(max_ms), gbps(min_ms)
-
 
 
 if __name__ == "__main__":
 
-    benchmark.run(print_data=True, show_plots=True)
-
     # Do some tests
+    dropout_fns = [
+        SeededVariationalDropout1D.apply,
+        SeededVariationalDropout2D.apply,
+    ]
     seed = random.randrange(int(1e6))
+    for dropout_fn in dropout_fns:
+        
+        x = torch.ones((14,18), device='cuda', dtype=torch.double, requires_grad=True)
+        #x = torch.randn(14, 18, dtype=torch.double, requires_grad=True, device='cuda')
+        y = dropout_fn(x, 0.5, seed)
 
-    #x = torch.ones((14,18), device='cuda', dtype=torch.double, requires_grad=True)
-    x = torch.randn(14, 18, dtype=torch.double, requires_grad=True, device='cuda')
-    y = variational_dropout(x, 0.5, seed)
+        torch.set_printoptions(precision=3, threshold=1000, linewidth=240)
 
-    torch.set_printoptions(precision=3, threshold=1000, linewidth=240)
+        print(x)
+        print(y)
 
-    print(x)
-    print(y)
+        # gradient check
+        _fn = lambda input_: dropout_fn(input_, 0.5, seed)
+        result = torch.autograd.gradcheck(_fn, x, eps=1e-6, atol=1e-4)
+        print(f"grad check: {result}")
 
-    # gradient check
-    dropout_fn = lambda input_: variational_dropout(input_, 0.5, seed)
-    result = torch.autograd.gradcheck(dropout_fn, x, eps=1e-6, atol=1e-4)
-    print(f"grad check: {result}")
+
+    # test performance
+    _benchmark.run(print_data=True, save_path='./performance_report/')
+    
+    import pprint
+    for key, value in _variational_dropout_kernel_1D.cache.items(): 
+        print(f"{key}: {value.kwargs}")
+    for key, value in _variational_dropout_kernel_2D.cache.items(): 
+        print(f"{key}: {value.kwargs}")
+
+
 
